041fe254986e637252e9b798098d6538c34cbd5c
claudsaints <claudiodsantoss7@gmail.com>
2025-02-15 22:17:19 -0300

    projects image adjust

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -3,7 +3,6 @@
 
 import { FaLink, FaGithub } from "react-icons/fa";
 import { useMemo } from "react";
-import Image from "next/image";
 
 interface ProjectProps {
   project: {
@@ -26,7 +25,7 @@ function ProjectCard({ project }: ProjectProps) {
     return (
 
       <div className="project mb-2 gap-4">
-        <a href={repoUrl} target="_blank" rel="noreferrer"><Image src={thumbnail + ".png"} className="zoom h-full w-full" alt="thumbnail" /></a>
+        <a href={repoUrl} target="_blank" rel="noreferrer"><img src={thumbnail + ".png"} className="zoom" alt="thumbnail" height="100%" width="100%" /></a>
         <a className="no-underline mt-5" href={repoUrl} target="_blank" rel="noreferrer">
           <h2 className="text-3xl mt-4 uppercase font-bold">{title}</h2>
         </a>
